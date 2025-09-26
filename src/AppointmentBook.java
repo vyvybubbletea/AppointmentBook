f6ce75de2cf1cfc7c622ae4f3946644890360f53
Ivy Li <ivyl68@nycstudents.net>
2025-09-26 15:35:50 -0400

    2nd

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
-public class AppointmentBook {
+public class AppointmentBook
+{
+    private boolean [][] schedule;
+    public AppointmentBook(boolean [][] schedule)
+    {
+        this.schedule = schedule;
+    }
+    private boolean isMinuteFree(int period, int minute)
+    {
+        return schedule[period - 1][minute];
+    }
+    public int findFreeBlock(int period, int duration)
+    {
+        return 0;
+    }
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        return false;
+    }
+   public void printPeriod(int period)
+   {
+        for(int i = 0; i < schedule[period - 1].length; i++)
+            System.out.println(i + " " + schedule[period - 1][i]);
+   }
+   public int findFreeBlock(int period, int duration)
+   {
+       int block = 0;
+       for(int i = 0; i < 60; i++)
+           if(isMinuteFree(period, i))
+           {
+               block++;
+               if(block == duration);
+           }
+   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class Main {
+public class Main
+{
+    boolean[][] schedule = new boolean[8][60];
+    AppointmentBook a = new AppointmentBook(schedule);
+    for(int i = 10; i < 15; i++) schedule[1][i] = true;
+    a.printPeriod(2);
 }
